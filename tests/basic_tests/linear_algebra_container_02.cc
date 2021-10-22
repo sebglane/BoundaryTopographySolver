e8a3e08d35eb77a36a63a3720a6d04fc9e955af3
Sebastian Glane <glane@tu-berlin.de>
2021-10-22 19:42:08 +0200

    Added missing include

--- tests/basic_tests/linear_algebra_container_02.cc
+++ tests/basic_tests/linear_algebra_container_02.cc
@@ -21,6 +21,7 @@
 
 #include <deal.II/grid/grid_generator.h>
 
+#include <deal.II/lac/trilinos_sparsity_pattern.h>
 #include <deal.II/lac/trilinos_sparse_matrix.h>
 #include <deal.II/lac/trilinos_vector.h>
 
